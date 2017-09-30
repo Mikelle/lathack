75eb01984de0289c5dab50264de7c3d2af934228
bulgakovk <kbulgakov@yahoo.com>
2017-09-30 19:23:38 +0300

    Merge branch 'master' of /Users/a123/lathack with conflicts.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,14 @@
-var Sportsman = artifacts.require('./Sportsman.sol');
+const Sportsman = artifacts.require('./Sportsman.sol');
+const Proposal = artifacts.require("./Proposal.sol");
+const PayableProposal = artifacts.require("./PayableProposal.sol");
+const FinalizableProposal = artifacts.require("./FinalizableProposal.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Sportsman);
+    deployer.deploy(Sportsman);
+    // deployer.deploy(PayableProposal);
+    // deployer.deploy(FinalizableProposal);
+    // deployer.link(PayableProposal, Proposal);
+    // deployer.link(FinalizableProposal, Proposal);
+    //
+    // deployer.deploy(Proposal, false, 20, "title", "description");
 };
