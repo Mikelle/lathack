1475cd234bc3dfc65d626a3b2d2fad56f6641e33
Mikelle <mikhwall@gmail.com>
2017-10-01 15:29:04 +0300

    Implemented IIdenity.sol

--- contracts/asset/IAuthority.sol
+++ contracts/asset/IAuthority.sol
@@ -7,15 +7,25 @@ contract IAuthority {
 	function isCapableForIdentity(IIdentity identity) {
 
 	}
+
 	function isCapableForAsset(LiquidAsset asset) {
 
 	}
 
-	function canSell(IIdentity identity, LiquidAsset asset) {
-		
+	function canSell(IIdentity identity, LiquidAsset asset) returns (bool) {
+		if (!identity.getKYCCompleted()) {
+			return false;
+		} 
+
+		if (asset.balanceOf(msg.sender) == 0) {
+			return false;
+		}
 	}
 
-	function canBuy(IIdentity identity, LiquidAsset asset) {
+	function canBuy(IIdentity identity, LiquidAsset asset) returns (bool) {
+		if (!identity.getKYCCompleted()) {
+			return false;
+		} 
 
 	}
 }
